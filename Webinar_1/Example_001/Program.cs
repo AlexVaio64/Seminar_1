a4fb52a058ec1a2d886bea81d582a2ba5026990e
Michel Nekrasov <moscou@inbox.ru>
2022-10-13 20:41:29 +0300

    Kvadrat celogo chisla

---
+++ Webinar_1/Example_001/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Это программа вычисления квадрата числа");
+Console.WriteLine("Введите число:");
+int input = int.Parse(Console.ReadLine()!);
+int result = input * input;
+Console.WriteLine("Квадрат числа: " + result);
